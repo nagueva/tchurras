a8eab056615990a3be75db72d0c1f1794235025e
Guilherme Nagüeva <guilherme@nagueva.com>
2017-01-05 12:02:52 -0200

    New JS functions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,25 +1,76 @@
-function mark(item){
+function setHeight() {
+  mh = $('#main').height();
+  rh = $('#results').height();
+  baseHeight = Math.max(mh,rh);
+  $('#container')
+    .add('#main')
+    .add('#results')
+    .height(baseHeight);
+}
+function randomIcons(){
+  n = Math.floor(Math.random()*20)+1;
+  $('#male-icon').attr('src', '/assets/img/avatar-man-'+n+'.svg');
+  n = Math.floor(Math.random()*15)+1;
+  $('#female-icon').attr('src', '/assets/img/avatar-woman-'+n+'.svg');
+}
+function moveTo(target){
+  if(target == '#results'){
+    y = - baseHeight;
+  } else {
+    y = 0;
+  }
+  $('#main').css('margin-top', y);
+  $('#main')
+    .add('#results')
+    .toggleClass('waiting');
+}
+function showResults(){
+  mn = Number($('#men').val());
+  wn = Number($('#women').val());
+  t = mn + wn;
+  $('#total').html(t);
+  $('#total-men').html(mn);
+  $('#total-women').html(wn);
+  $('#male-icon').attr('title', mn+' homens');
+  $('#female-icon').attr('title', wn+' mulheres');
+  randomIcons();
+  moveTo('#results');
+}
+function checkItem(item){
   $(item)
     .add(item+'>.unity>a.checkbox')
     .toggleClass('active');
 }
 var core = {
   init:function(){
-    console.log('init:starting');
+    $('#men').focus();
+    setHeight();
     $('#number-of .actions>a.plus').click(function(){
+      input = $(this).attr('href');
+      v = $(input).val() ? $(input).val() : 0;
+      if($(input).val()<$(input).attr('max')){v++;}
+      $(input).val(v);
       return false;
     });
     $('#number-of .actions>a.minus').click(function(){
+      input = $(this).attr('href');
+      v = $(input).val() ? $(input).val() : 0;
+      if($(input).val()>0){v--;}
+      $(input).val(v);
       return false;
     });
     $('#number-of').submit(function(){
+      showResults();
+      return false;
+    });
+    $('#results .reset-form').click(function(){
+      moveTo('#main')
       return false;
     });
     $('#items>.item>.unity>a.checkbox').click(function(){
-      mark($(this).attr('href'));
+      checkItem($(this).attr('href'));
       return false;
     })
-    console.log('init:ok!');
   }
 }
 $(function(){
