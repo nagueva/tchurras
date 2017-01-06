015d422b4d2bae5526527269dfae045ee69ee004
Guilherme Nagüeva <guilherme@nagueva.com>
2017-01-06 19:02:57 -0200

    Improvements and final calc
    Fix #6 #7 #9

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,26 @@
+item = {};
+ms = [
+  'homens',
+  'carinhas',
+  'camaradas',
+  'queridões',
+  'skywalkers',
+  'garotos',
+  'manos',
+  'manolos'
+];
+ws = [
+  'mulheres',
+  'queridinhas',
+  'amigas',
+  'garotas',
+  'minas',
+  'lindas'
+];
+errors = [
+  'Opa, esse churrasco precisa ter pelo menos uma pessoa.',
+  'Ops, você precisa informar um número válido.'
+];
 function setHeight() {
   mh = $('#main').height();
   rh = $('#results').height();
@@ -8,9 +31,9 @@ function setHeight() {
     .height(baseHeight);
 }
 function randomIcons(){
-  n = Math.floor(Math.random()*20)+1;
+  n = Math.floor(Math.random()*18)+1;
   $('#male-icon').attr('src', '/assets/img/avatar-man-'+n+'.svg');
-  n = Math.floor(Math.random()*15)+1;
+  n = Math.floor(Math.random()*14)+1;
   $('#female-icon').attr('src', '/assets/img/avatar-woman-'+n+'.svg');
 }
 function moveTo(target){
@@ -24,17 +47,49 @@ function moveTo(target){
     .add('#results')
     .toggleClass('waiting');
 }
-function showResults(){
-  mn = Number($('#men').val());
-  wn = Number($('#women').val());
+function validate() {
+  if($('#men').is(':invalid')||$('#women').is(':invalid')){
+    showErrors(1);
+  } else{
+    mn = Number($('#men').val());
+    wn = Number($('#women').val());
+    if(mn==0&&wn==0){
+      showErrors(0);
+    } else {
+      $('#form-msgs').slideUp();
+      showResults(mn, wn);
+    }        
+  }
+}
+function showErrors(code){
+  $('#form-msgs').html(errors[code]).slideDown();
+  if(code==0){
+    target = $('#men');
+  } else if(code = 1){
+    target = $('#men:invalid').add('#women:invalid');
+  }
+  target.focus();
+}
+function showResults(mn, wn){
+  item['meat'] = (.350*mn)+(.250*wn);
+  item['sausage'] = (.150*mn)+(.100*wn);
+  item['bread'] = (3*mn)+(2*wn);
+  item['beer'] = (1.5*mn)+(.700*wn);
+  item['soda'] = (.500*mn)+(.750*wn);
+  item['charcoal'] = (.300*mn)+(.250*wn);
+  // item[''] = (*mn)+(*wn);
   t = mn + wn;
   $('#total').html(t);
   $('#total-men').html(mn);
   $('#total-women').html(wn);
-  $('#male-icon').attr('title', mn+' homens');
-  $('#female-icon').attr('title', wn+' mulheres');
   randomIcons();
+  $('#male-icon').attr('title', mn+' '+ms[Math.floor(Math.random() * ms.length)]);
+  $('#female-icon').attr('title', wn+' '+ws[Math.floor(Math.random() * ws.length)]);
+  $.each(item, function(index, value) { 
+    $('#'+index+'>.unity>span.value').html(Math.round(value*100)/100);
+  });
   moveTo('#results');
+  $('#form-msgs').slideUp();
 }
 function checkItem(item){
   $(item)
@@ -60,7 +115,7 @@ var core = {
       return false;
     });
     $('#number-of').submit(function(){
-      showResults();
+      validate();
       return false;
     });
     $('#results .reset-form').click(function(){
